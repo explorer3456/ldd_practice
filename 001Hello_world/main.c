c39d5e70c5b3350381f1a51efa9a4bfac2b7c272
Ditto Kim <rlawhdgus34@gmail.com>
2024-07-21 22:08:19 +0900

    Bug report : kernel log level doesnt work
    - even though I changed the kernel log level from debug to err, it doesnt print

--- 001Hello_world/main.c
+++ 001Hello_world/main.c
@@ -2,7 +2,11 @@
 
 static int __init hello_world_init(void)
 {
-	pr_info("Hello world\n");
+	printk(KERN_INFO "Hello world\n");
+	printk(KERN_WARNING "warning Hello world\n");
+	printk(KERN_ERR  "err Hello world\n");
+	printk(KERN_DEBUG "deubg Hello world\n");
+	pr_err("pr err test");
 	return 0;
 }
 
