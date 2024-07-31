800f6df845e793b26824265617dd1831c48c76a5
Ditto Kim <rlawhdgus34@gmail.com>
2024-08-01 01:42:55 +0900

    implement container of macro

--- container_of/main.c
+++ container_of/main.c
@@ -21,6 +21,10 @@ void * get_my_container(void *member_addr)
 	return base_addr;
 }
 
+#define my_container_of( ptr, name ,type) ({	\
+		size_t offset = (size_t)&(((type *)0)->name); \
+		(type *)((void *)ptr - offset); \
+		})
 
 int main(void)
 {
@@ -49,7 +53,8 @@ int main(void)
 
 	printf("a is : %d\n", *(int *)ptr);
 
-	temp_ptr = get_my_container( & _data.c);
+//	temp_ptr = get_my_container( & _data.c);
+	temp_ptr = my_container_of( &_data.c, c, struct some_data );
 
 	printf("new a is : %d\n", temp_ptr->a);
 	
